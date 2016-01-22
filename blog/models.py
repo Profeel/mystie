be5555958534a4d4bd11785895a010a8ff386bea
Profeel <cqupt_wq@hotmail.com>
2016-01-22 15:48:47 +0800

    add something in models

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
-    publish_time = models.DateTimeField()
+    publish_time = models.DateTimeField(auto_now= True)
     body = models.TextField(default='')
     tag = models.ForeignKey('Tag')
     
