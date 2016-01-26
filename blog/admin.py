51b3408024eb0388dfb91341cf925dd3df81976c
Profeel <cqupt_wq@hotmail.com>
2016-01-26 08:04:52 +0800

    add search into admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-”
 from django.contrib import admin
 from blog.models import BlogPost,Tag
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title','publish_time',)
+    # 添加搜索项，按照title进行搜索
+    search_fields = ['title']
     
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title',)
